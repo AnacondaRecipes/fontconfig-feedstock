56150c2554c9769e263dc9ab2c63c49229a088de
Ray Donnelly <mingw.android@gmail.com>
2017-08-31 05:50:30 -0700

    Add ${PREFIX}/fonts to the fonts path

--- recipe/build.sh
+++ recipe/build.sh
@@ -13,10 +13,11 @@ sed -i.orig s:'@PREFIX@':"${PREFIX}":g src/fccfg.c
 
 chmod +x configure
 
-./configure --prefix "${PREFIX}"  \
-	    --enable-libxml2      \
-	    --enable-static       \
-	    --disable-docs
+./configure --prefix "${PREFIX}"              \
+            --enable-libxml2                  \
+            --enable-static                   \
+            --disable-docs                    \
+            --with-add-fonts=${PREFIX}/fonts
 
 make -j${CPU_COUNT} ${VERBOSE_AT}
 eval ${LIBRARY_SEARCH_VAR}="${PREFIX}/lib" make check
