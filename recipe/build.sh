69d185dc7acd6c687b4445cd082ee137ec8cf215
Cheng H. Lee <clee@anaconda.com>
2021-02-04 06:33:16 -0600

    Fix 2.13.1 build on macOS

--- recipe/build.sh
+++ recipe/build.sh
@@ -9,9 +9,14 @@ autoreconf -vfi
 # https://github.com/Homebrew/homebrew-core/blob/master/Formula/fontconfig.rb
 # https://bugs.freedesktop.org/show_bug.cgi?id=105366
 if [[ ${target_platform} == osx-64 ]]; then
+  # NOTE: conda-forge's recipe does not include the next three lines to avoid
+  # linking to their libuuid, but that may be due to differences between their
+  # `util-linux` version and our `libuuid`-project one.
   export UUID_CFLAGS=" "
   export UUID_LIBS=" "
-  sed -i -e 's|PKGCONFIG_REQUIRES_PRIVATELY=\"\$PKGCONFIG_REQUIRES_PRIVATELY uuid\"||g' configure
+  # `true` here to avoid error in shell conditional.
+  sed -i -e 's|PKGCONFIG_REQUIRES_PRIVATELY=\"\$PKGCONFIG_REQUIRES_PRIVATELY uuid\"|true|g' configure
+
   FONT_DIRS=--with-add-fonts="${PREFIX}"/fonts,/System/Library/Fonts,/Library/Fonts,~/Library/Fonts,/System/Library/Assets/com_apple_MobileAsset_Font3,/System/Library/Assets/com_apple_MobileAsset_Font4
 else
   FONT_DIRS=--with-add-fonts="${PREFIX}"/fonts
