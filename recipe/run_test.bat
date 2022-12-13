db79a340faeb4f72a822ee0c62467082d737154a
Kai Tietz <ktietz@anaconda.com>
2022-12-13 13:05:25 +0100

    remove debugging ...

--- recipe/run_test.bat
+++ recipe/run_test.bat
@@ -13,5 +13,5 @@ echo "Testing for presence of libfontconfig in build output"
 if not exist %PREFIX%/Library\lib\fontconfig.lib exit /b 1 
 if not exist %PREFIX%\Library\bin\fontconfig-1.dll exit /b 1
 
-dir %PREFIX%\Library\lib\pkgconfig
+rem check for pkgconfig file ...
 if not exist %PREFIX%\Library\lib\pkgconfig\fontconfig.pc exit /b 1
