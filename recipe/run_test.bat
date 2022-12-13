6627dabd17b048141b9226a1573698b96159a046
Kai Tietz <ktietz@anaconda.com>
2022-12-13 12:44:58 +0100

    narrow down .pc file on windows

--- recipe/run_test.bat
+++ recipe/run_test.bat
@@ -15,5 +15,6 @@ echo "Testing for presence of libfontconfig in build output"
 if not exist %PREFIX%/Library/lib/fontconfig.lib exit /b 1 
 if not exist %PREFIX%/Library/bin/fontconfig-1.dll exit /b 1
 
-dir %PREFIX%/Library/lib
-if not exist %PREFIX%/Library/lib/pkgconfig/fontconfig.pc exit /b 1
+dir %PREFIX%\Library
+dir %PREFIX%\Library\lib
+if not exist %PREFIX%/Library/pkgconfig/fontconfig.pc exit /b 1
