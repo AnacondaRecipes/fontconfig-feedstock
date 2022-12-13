ed57eb4eed0d33daca918fe4008a99512b657c73
Kai Tietz <ktietz@anaconda.com>
2022-12-13 12:52:30 +0100

    adjust run_test.bat for windows ...

--- recipe/run_test.bat
+++ recipe/run_test.bat
@@ -9,12 +9,9 @@ fc-scan --help
 fc-validate --help
 
 REM Test for libraries.
-echo "Testing for presence of libfontconfig.a in build output"
-test -f "${PREFIX}/lib/libfontconfig.a"
 echo "Testing for presence of libfontconfig in build output"
-if not exist %PREFIX%/Library/lib/fontconfig.lib exit /b 1 
-if not exist %PREFIX%/Library/bin/fontconfig-1.dll exit /b 1
+if not exist %PREFIX%/Library\lib\fontconfig.lib exit /b 1 
+if not exist %PREFIX%\Library\bin\fontconfig-1.dll exit /b 1
 
-dir %PREFIX%\Library
-dir %PREFIX%\Library\lib
-if not exist %PREFIX%/Library/pkgconfig/fontconfig.pc exit /b 1
+dir %PREFIX%\Library\lib\pkgconfig
+if not exist %PREFIX%\Library\lib\pkgconfig\fontconfig.pc exit /b 1
